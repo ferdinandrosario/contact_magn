e0a81ce1bad10f3faa8a5fb9bd4016989bb1b13a
Ferdinand Rosario <ferdinandrosario@gmail.com>
2016-03-10 05:19:41 +0530

    Readme added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+root 'contacts#index'
 resources :contacts, only: [:index, :new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
